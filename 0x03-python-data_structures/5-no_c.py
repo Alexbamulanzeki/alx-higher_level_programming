0c458f8fff00d2d4b4825d55e6668a168e60ad27
Alexbamulanzeki <syliviakimuli2@gmail.com>
2021-09-10 07:17:35 -0700

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    new_string = ""
+    for i in range(0, len(my_list)):
+        if my_list[i]!= 'c' and my_list[i]!= 'C':
+            new_string+= my_string[i]
+            return new_string
+        
