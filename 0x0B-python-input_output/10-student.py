9d17491edd6daa85de12d356baac2546f8630f26
Alexbamulanzeki <syliviakimuli2@gmail.com>
2021-10-12 08:47:43 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-''' class Student that defines a student
-'''
+"""
+a class Student that defines a student by: (based on 11-student.py)
+"""
 
 
 class Student:
-    '''module class student
-    '''
-
+    """
+    a class Student that defines a student
+    """
     def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
+        """
+        Instantiation with first_name, last_name and age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Public method that retrieves a dictionary
+        representation of a Student instance
+        """
+        if attrs is not None:
+            return {i: j for i, j in self.__dict__.items() if i in attrs}
+        else:
+            return self.__dict__
